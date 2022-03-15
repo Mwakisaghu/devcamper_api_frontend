909581125472b75f094f7c025c9bca2de5b941b4
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-15 15:03:15 +0300

    This installs redux, redux-thunk & creates redux store file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import Login from './components/layout/Login';
 import './App.css';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
