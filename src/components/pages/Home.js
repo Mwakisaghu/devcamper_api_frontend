a23eaf7e9fd971fd42165d9de1aa9d77751b2af4
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-30 21:51:20 +0300

    This adds Home & About pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
+
+const Home = () => {
+  return (
+    <div id="container" style={{ padding: '24px' }}>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+
+          <ul style={{ float: 'right' }}>
+            <li>
+              <Link to="/login">Login</Link>{' '}
+              <Link to="/register">Register</Link>{' '}
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Bootcamps</Breadcrumb.Item>
+            <Breadcrumb.Item>Courses</Breadcrumb.Item>
+            <Breadcrumb.Item>Reviews</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Devcamper_api_frontend ©2018 Created by Harrison
+        </Footer>
+      </Layout>
+    </div>
+  );
+};
+
+export default Home;
