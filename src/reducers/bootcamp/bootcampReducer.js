0416aa5c9feca28cabdf901de1f0621ab5f4019e
mwakisaghu <harrisonchambela2016@gmail.com>
2022-04-04 20:24:23 +0300

    This creates a bootcampReducer, imports bootcamp types & sets the initial state for the bootcamps

---
+++ src/reducers/bootcamp/bootcampReducer.js
@@ -0,0 +1,19 @@
+import { GET__BOOTCAMPS } from '../../actions/types';
+
+const initialState = {
+  bootcamp: [],
+  error: null,
+};
+
+/* eslint-disable */
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET__BOOTCAMPS:
+      return {
+        ...state,
+        bootcamp: action.payload.bootcamp,
+      };
+    default:
+      return state;
+  }
+};
