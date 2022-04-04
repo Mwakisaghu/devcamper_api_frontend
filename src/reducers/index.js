5971eea1e82cb74edda08458200ccc4f5267e723
mwakisaghu <harrisonchambela2016@gmail.com>
2022-04-04 19:58:54 +0300

    This imports bootcampReducer into our root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducers from './auth/userReducers';
+import bootcampReducer from './bootcamp/bootcampReducer';
 
 export default combineReducers({
   user: userReducers,
+  bootcamp: bootcampReducer,
 });
