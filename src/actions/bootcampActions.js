844fc84085545354583bdcd4f2bc72820a4785c1
mwakisaghu <harrisonchambela2016@gmail.com>
2022-04-28 10:09:23 +0300

    This imports create bootcamp type and shows create bootcamp action which uses axios to create a new bootcamp

--- src/actions/bootcampActions.js
+++ src/actions/bootcampActions.js
@@ -1,4 +1,8 @@
-import { BOOTCAMPS_ERROR, GET__BOOTCAMPS } from '../actions/types';
+import {
+  BOOTCAMPS_ERROR,
+  GET__BOOTCAMPS,
+  CREATE_BOOTCAMP,
+} from '../actions/types';
 import axios from 'axios';
 
 //Get all bootcamps
@@ -10,6 +14,29 @@ export const getBootcamps = (token) => async (dispatch) => {
 
     dispatch({
       type: GET__BOOTCAMPS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: BOOTCAMPS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+//Create new Bootcamp
+export const createBootcamp = (token) => async (dispatch) => {
+  try {
+    console.log('I was here...');
+
+    const res = await axios.post('/api/v1/bootcamps', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    dispatch({
+      type: CREATE_BOOTCAMP,
       payload: res.data,
     });
   } catch (err) {
