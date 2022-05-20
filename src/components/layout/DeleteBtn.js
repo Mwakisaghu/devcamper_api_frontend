201de650476bf89810d45d969bed19fe8bcf586a
mwakisaghu <harrisonchambela2016@gmail.com>
2022-05-20 11:09:59 +0300

    This shows the deleteBtn/component and introduces useDispatch hook - uses axios delete method to delete a bootcamp

--- src/components/layout/DeleteBtn.js
+++ src/components/layout/DeleteBtn.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Button } from 'antd';
+import { connect, useDispatch } from 'react-redux';
+import { deleteBootcamp } from '../../actions/bootcampActions';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const DeleteBtn = () => {
+const DeleteBtn = ({ deleteBootcampAction, token, _id }) => {
+  const dispatch = useDispatch();
+
+  const onDelete = async (e) => {
+    e.preventDefault();
+
+    console.log(token, _id);
+
+    const res = await axios({
+      method: 'delete',
+      url: `/api/v1/bootcamps/${_id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    });
+    console.log({ res });
+    if (res.status === 200) {
+      dispatch(deleteBootcamp(_id));
+    }
+  };
+
   return (
     <div>
-      <Button size="small">
+      <Button size="small" onClick={(e) => onDelete(e)}>
         <i
           class="material-icons"
           style={{ color: 'red', zIndex: 10 }}
@@ -17,4 +42,14 @@ const DeleteBtn = () => {
   );
 };
 
-export default DeleteBtn;
+DeleteBtn.propTypes = {
+  deleteBootcamp: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  token: state.user.token,
+});
+
+export default connect(mapStateToProps, {
+  deleteBootcampAction: deleteBootcamp,
+})(DeleteBtn);
